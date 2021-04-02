b8ba6f21368c7678cf608ecf57191f0c0a1c4c83
Jingzhen Liu <jingzhen_liu1990@hotmail.com>
2021-04-02 22:04:40 +0100

    Update NS3Factor model

--- YieldCurveModelling/YieldCurveModelling/YieldCurveModels/NS3FactorModel.cs
+++ YieldCurveModelling/YieldCurveModelling/YieldCurveModels/NS3FactorModel.cs
@@ -3,11 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using YieldCurveModelling.PSOAlgorithm;
 namespace YieldCurveModelling.YieldCurveModels
 {
-    public class NS3FactorModel
+    public class StaticNS3FactorModel
     {
         //Implement Nelson-Siegal 3 factor model
+        public double beta1 { get; set; }
+        public double beta2 { get; set; }
+        public double beta3 { get; set; }
+        public double lambda { get; set; }
+        public double[] tau { get; set; }
+        public double[] GetYield()
+        {
+            var result = new double[tau.Length];
+            for (int i = 0; i < tau.Length; i++)
+            {
+                var scalefactor = tau[i] / lambda;
+                var a = (1 - Math.Exp(-scalefactor)) / scalefactor;
+                var b = a - Math.Exp(-scalefactor);
+
+                result[i] = beta1 + beta2 * a + beta3 * b;
+            }
+            return result;
+        }
+    }
+    public class StaticNS3FactorModelCalibration
+    { 
+        public double[] yields { get; set; }
+        public double[] maturities { get; set; }
+
+        public double[] Calibration()
+        {
+            var PSO = new PSOOptimization();
+            var lowerbound = new double[4] { 0.00001, -99.99, -99.99, 0.00001 };
+            var upperbound = new double[4] { 99.99, 99.99, 99.99, 99.99 };
+            PSO.lowerbound = lowerbound;
+            PSO.upperbound = upperbound;
+            PSO.maximumiteration = 5000;
+            PSO.numofswarms = 200;
+            PSO.inertiaweightmax = 1.2;
+            PSO.inertiaweightmin = 0.1;
+            PSO.objectfun = Objfun;
+            PSO.tolerance = 0.0000001;
+            PSO.Vmax = 4;
+            PSO.c1 = 2;
+            PSO.c2 = 2;
+            PSO.chi = 0.73;
+            var optimizedp = PSO.Optimize();
+            return optimizedp;
+        }
+        public double Objfun(double[]para)
+        {
+            var sns3factor = new StaticNS3FactorModel();
+            sns3factor.beta1 = para[0];
+            sns3factor.beta2 = para[1];
+            sns3factor.beta3 = para[2];
+            sns3factor.lambda = para[3];
+            sns3factor.tau = maturities;
+            var modelyields = sns3factor.GetYield();
+            var error = 0.0;
+            for (int i = 0; i < yields.Length; i++)
+            {
+                var temperror = (modelyields[i] - yields[i]) / (yields[i] + 0.000000000000001);
+                error = error + temperror * temperror;
+            }
+            return error;
+        }
     }
 }
