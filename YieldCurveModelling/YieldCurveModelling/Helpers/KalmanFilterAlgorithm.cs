a92bfcdc6aa71fb5ebbd4631f494f2f866e39aa7
Liu, Jingzhen (Reigate) <jingzhen.liu@towerswatson.com>
2021-04-07 14:53:48 +0100

    Add Kalman filter class

---
+++ YieldCurveModelling/YieldCurveModelling/Helpers/KalmanFilterAlgorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace YieldCurveModelling.Helpers
+{
+    public class KalmanFilterAlgorithm
+    {
+        public Matrix<double> initialstate { get; set; }
+        public Matrix<double> statetransition { get; set; }
+        public Matrix<double> statenoisecovariance { get; set; }
+        public Matrix<double> observationmodel { get; set; }
+        public Matrix<double> observationnoisecov { get; set; }
+        public Matrix<double> observation { get; set; }
+        public Dictionary<int, Matrix<double>> FilterStates()
+        {
+            var results = new Dictionary<int, Matrix<double>>();
+
+            return results;
+        }
+    }
+}
